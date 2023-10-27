7ea584227d1743f3830835385edd6354b00cded7
Sambalicious <stemitope370@gmail.com>
2023-10-27 22:34:40 +0100

    fix: fix eslint build issue
    Installed missing dependecy causing build error

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,5 @@
 import {
+  Loader2,
   MoveLeft,
   Search,
   SlidersHorizontal,
@@ -11,4 +12,5 @@ export const Icons = {
   Filter: SlidersHorizontal,
   Search,
   Back: MoveLeft,
+  Spinner: Loader2,
 };
