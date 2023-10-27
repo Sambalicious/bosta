554fc7adf0c4165aa760eb179c22e78b93e4ddbb
Sambalicious <stemitope370@gmail.com>
2023-10-27 22:59:28 +0100

    chore: added middleware to redirect from / to /books

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,22 @@
-export default function middleware() {}
+import { NextResponse, type NextRequest } from "next/server";
+
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  if (pathname === "/") {
+    const response = NextResponse.redirect(new URL("/books", req.url));
+    return response;
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
